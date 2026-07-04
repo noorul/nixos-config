123672535c69bedf8d7700fbcf78538721bf7463
Noorul Islam K M <noorul@noorul.com>
2026-07-04 07:09:51 +0530

    Enable hardware graphics

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -38,4 +38,8 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = true;
+  hardware.nvidia.modesetting.enable = true;
 }
